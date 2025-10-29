2929a477f4e48f1483d99ef66fde54c8f7690479
KimTun <kim.tunturi@gmail.com>
2025-10-29 23:53:59 +0200

    pöytäkone

--- moduuli_12/12.2.py
+++ moduuli_12/12.2.py
@@ -2,10 +2,12 @@ import requests
 import json
 
 
-apikey ="22cad3a7f5cb97a3610bd42ddd60b0a3"
+apikey ="input your own apikey here"
 city_name = input("City: ")
 
-request = f"https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${apikey}&units=metric"
+request = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={apikey}&units=metric"
 response = requests.get(request).json()
-print(json.dumps(response, indent=4))
+
+print(response["weather"][0]["main"])
+print(f"{response['main']['temp']} °C")
 
